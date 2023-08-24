f4beafb31343bc4e2d361330335ba1f328840ca7
wonsj <windymorning@naver.com>
2023-08-24 21:26:46 +0900

    [WONSJ-2] merge conflict

--- src/main/kotlin/com/mytest/app/service/SearchService.kt
+++ src/main/kotlin/com/mytest/app/service/SearchService.kt
@@ -3,130 +3,22 @@ package com.mytest.app.service
 import com.mytest.app.domain.KakaoResponse
 import com.mytest.app.domain.NaverResponse
 import com.mytest.app.domain.SearchResult
-import com.mytest.app.repository.DataRedisRepository
-import com.mytest.app.repository.SearchRepository
-import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import reactor.core.scheduler.Schedulers
 
+interface SearchService  {
 
-@Service
-class SearchService(
-	val searchRepository: SearchRepository,
-	val dataRedisRepository: DataRedisRepository
-) {
+//	suspend fun searchKakaoResult(keyword: String, page: String, size: String): Mono<KakaoResponse>
 
-	private val logger = LoggerFactory.getLogger(this::class.java)
+//	suspend fun searchNaverResult(keyword: String, page: String, size: String): Mono<NaverResponse>
 
-	/**
-	 *
-	 */
-	suspend fun searchKakaoResult(keyword: String, page: String, size: String): Mono<KakaoResponse> {
+	suspend fun sortSearchResult(keyword: String, rank: String, page: String, size: String): Mono<SearchResult>
 
-		return searchRepository.getKakaoSearchResult(keyword, page, size)
-	}
+//	suspend fun getAllValues(): Flux<Any>
 
+//	suspend fun saveKeyValue(keyword: String, value: Int): Mono<Boolean>
 
-	/**
-	 * 네이버 검색 API
-	 */
-	suspend fun searchNaverResult(keyword: String, page: String, size: String): Mono<NaverResponse> {
+//	suspend fun getScoreByValue(rank: String, keyword: String): Mono<Double>
 
-		return searchRepository.getNaverSearchResult(keyword, page, size)
-	}
-
-
-	suspend fun sortSearchResult(keyword: String, page: String, size: String): Mono<SearchResult> {
-
-		val kakaoList = searchRepository.getKakaoSearchResult(keyword, page, size)
-			.map { it.documents }
-
-		val naverList = searchRepository.getNaverSearchResult(keyword, page, size)
-			.map { it.items }
-
-		val mergeValues = mergeValuesFromWebClient(kakaoList, naverList)
-
-		logger.debug("--------> getCount start")
-
-		dataRedisRepository.get(keyword)
-			.flatMap { countValue ->
-				val newCount = (countValue.toString().toIntOrNull() ?: 0) + 1
-				dataRedisRepository.save(keyword, newCount).thenReturn(newCount)
-			}
-			.defaultIfEmpty(0) // Set default value if count is empty
-			.flatMap { currentCount ->
-				if (currentCount > 0) {
-					Mono.just(currentCount)
-				} else {
-					dataRedisRepository.save(keyword, 1).thenReturn(1)
-				}
-			}
-			.onErrorResume { Mono.error(it) }
-			.subscribe { logger.info("--------> count : {}", it) }
-
-
-
-		return mergeValues
-			.subscribeOn(Schedulers.single())
-			.flatMap { titles ->
-				val titleObjects = titles.map { it?.let { it1 -> SearchResult.Title(it1) } }
-				Mono.just(SearchResult(titleObjects))
-			}
-	}
-
-	suspend fun mergeValuesFromWebClient(
-		kakaoList: Mono<List<KakaoResponse.Document>?>,
-		naverList: Mono<List<NaverResponse.Item>?>
-	)
-		: Mono<Set<String?>> {
-
-		val commonList = Mono.zip(kakaoList, naverList)
-			.map {
-				it.t1.map { it.placeName }
-					.intersect(it.t2.map { it.title })
-			}
-			.flatMap { Mono.just(it) }
-
-		val kakaos = Mono.zip(kakaoList, commonList)
-			.map {
-				it.t1.map { it.placeName }
-					.subtract(it.t2)
-			}
-			.flatMap { Mono.just(it) }
-
-		val navers = Mono.zip(naverList, commonList)
-			.map {
-				it.t1.map { it.title }
-					.subtract(it.t2)
-			}
-			.flatMap { Mono.just(it) }
-
-		return Mono.zip(commonList, kakaos, navers)
-			.map { it.t1.plus(it.t2).plus(it.t3) }
-			.flatMap { Mono.just(it) }
-			.switchIfEmpty(Mono.empty())
-			.onErrorResume { Mono.error(it) }
-	}
-
-	suspend fun getAllValues(): Flux<Any> {
-		return dataRedisRepository.getAll()
-	}
-
-
-	suspend fun saveKeyValue(keyword: String, value: Int): Mono<Boolean> {
-		return dataRedisRepository.save(keyword, value)
-	}
-
-
-	suspend fun getKeywordCount(keyword: String): Mono<Any> {
-
-		dataRedisRepository.get(keyword)
-			.subscribe { logger.info("--------> count : {}", it) }
-
-		return dataRedisRepository.get(keyword)
-	}
-
+	suspend fun getZsetValues(keyword: String): Flux<Any>
 }
-
